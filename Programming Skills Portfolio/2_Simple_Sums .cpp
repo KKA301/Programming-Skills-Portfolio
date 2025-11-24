f79de75089f95f724c37ec1e376f8d926624a582
KKA301 <lietuvaitiskarolis10@googlemail.com>
2025-11-24 09:43:38 +0000

    Update 2_Simple_Sums .cpp

--- Programming Skills Portfolio/2_Simple_Sums .cpp
+++ Programming Skills Portfolio/2_Simple_Sums .cpp
@@ -1,19 +1,9 @@
 #include <iostream> 
-
 using namespace std; 
 
-  
-
 int main() { 
-
     int number1 = 8;           
-
     int number2 = 10;         
-
     int sum = number1 + number2; 
-
-  
-
     cout << sum << endl;   
-
 } 

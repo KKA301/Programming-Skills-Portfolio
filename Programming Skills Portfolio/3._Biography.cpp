bcd0714d637ef5ecc33611c05ba6ef49a616c264
KKA301 <lietuvaitiskarolis10@googlemail.com>
2025-11-24 09:46:51 +0000

    3._Biography.cpp

--- Programming Skills Portfolio/3._Biography.cpp
+++ Programming Skills Portfolio/3._Biography.cpp
@@ -1,27 +1,12 @@
 #include <iostream> 
-
 #include <string> 
-
 using namespace std; 
 
-  
-
 int main() { 
-
-    string name = "Karolis Kaleda"; 
-
-    string hometown = "London"; 
-
-    int age = 25; 
-
-  
-
+    string name = "Karolis Kaleda"; //my full name
+    string hometown = "London"; //my city or hometown
+    int age = 25;// my actual age
     // Only one cout output as said in brief
-
     cout << name << "\n" << hometown << "\n" << age << endl; 
-
-  
-
     return 0; 
-
 } 

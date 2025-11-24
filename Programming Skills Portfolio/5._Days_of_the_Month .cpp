8fa3de3ce29a57f880f970ba2e723a3e53daa741
KKA301 <lietuvaitiskarolis10@googlemail.com>
2025-11-24 09:55:40 +0000

    5._Days_of_the_Month .cpp

--- Programming Skills Portfolio/5._Days_of_the_Month .cpp
+++ Programming Skills Portfolio/5._Days_of_the_Month .cpp
@@ -1,51 +1,27 @@
 #include <iostream> 
-
 using namespace std; 
 
-  
-
 int main() { 
-
     int m; 
-
     cout << "Month number: "; 
-
     cin >> m; 
 
-  
-
     switch(m){ 
-
         case 1: cout<<"31 days"; break; 
-
         case 2: cout<<"28 days"; break; 
-
         case 3: cout<<"31 days"; break; 
-
         case 4: cout<<"30 days"; break; 
-
         case 5: cout<<"31 days"; break; 
-
         case 6: cout<<"30 days"; break; 
-
         case 7: cout<<"31 days"; break; 
-
         case 8: cout<<"31 days"; break; 
-
         case 9: cout<<"30 days"; break; 
-
         case 10: cout<<"31 days"; break; 
-
         case 11: cout<<"30 days"; break; 
-
         case 12: cout<<"31 days"; break; 
-
         default: cout<<"Fake month"; 
-
     } 
-
-  
-
     return 0; 
+
 
 } 

3fa62e36da63478051f59dffcc049d63b8ea1ff3
KKA301 <lietuvaitiskarolis10@googlemail.com>
2025-11-24 09:38:51 +0000

    Update 10. _is _it_even .cpp

--- Programming Skills Portfolio/10. _is _it_even .cpp
+++ Programming Skills Portfolio/10. _is _it_even .cpp
@@ -2,32 +2,15 @@
 
 using namespace std; 
 
-  
-
 string checkEvenOdd(int num) { 
-
     if(num % 2 == 0) return "The provided number is even"; 
-
     else return "The provided number is odd"; 
-
 } 
 
-  
-
 int main() { 
-
     int number; 
-
     cout << "Enter a number: "; 
-
     cin >> number; 
-
-  
-
     cout << checkEvenOdd(number) << endl; 
-
-  
-
     return 0; 
-
 } 

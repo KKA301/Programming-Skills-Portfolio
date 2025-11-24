b25f39a95c7a2b0288fbb82c5f80648236b10659
KKA301 <lietuvaitiskarolis10@googlemail.com>
2025-11-24 10:03:06 +0000

    8._Simple_Search .cpp

--- Programming Skills Portfolio/8._Simple_Search .cpp
+++ Programming Skills Portfolio/8._Simple_Search .cpp
@@ -1,47 +1,21 @@
 #include <iostream> 
-
 #include <string> 
-
 using namespace std; 
 
-  
-
 int main() { 
-
     string names[] = {"Jake", "Zac", "Ian", "Ron", "Sam", "Dave"}; 
-
     string searchTerm = "Sam"; 
-
     bool found = false; 
-
-  
-
     for(int i = 0; i < 6; i++) { 
-
         if(names[i] == searchTerm) { 
-
             found = true; 
-
             break;   
-
         } 
-
     } 
-
-  
-
     if(found) { 
-
         cout << searchTerm << " was found " << endl; 
-
     } else { 
-
         cout << searchTerm << " was not found." << endl; 
-
     } 
-
-  
-
     return 0; 
-
 } 
